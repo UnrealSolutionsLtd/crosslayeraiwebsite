0dc0096b6c608bd7add68f52e28a6c5dfd65da42
Petr Leontev <raianknight@gmail.com>
2025-12-08 20:09:44 +0000

    Add GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Rajdhani, JetBrains_Mono } from 'next/font/google'
+import Script from 'next/script'
 import './globals.css'
+
+const GA_MEASUREMENT_ID = 'G-NQFCLZLB51'
 
 const rajdhani = Rajdhani({
   subsets: ['latin'],
@@ -31,6 +34,20 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </head>
       <body className={`${rajdhani.variable} ${jetbrainsMono.variable}`}>
         {children}
       </body>
